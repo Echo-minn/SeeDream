a8e9d690e5b0490f2bc70471fcb3de3e687c53ae
5+2 <43105848+diansandaosiwje@users.noreply.github.com>
2020-04-13 23:33:09 +0800

    Update data.source.js
    按古典音乐时期划分界面 移除了第二页

--- src/Concert/data.source.js
+++ src/Concert/data.source.js
@@ -6,11 +6,11 @@ export const Feature80DataSource = {
   titleWrapper: {
     className: 'feature8-title-wrapper',
     children: [
-      { name: 'title', className: 'feature8-title-h1', children: '使用流程' },
+      { name: 'title', className: 'feature8-title-h1', children: '古典主义' },
       {
         name: 'content',
         className: 'feature8-title-content',
-        children: '流程简单清晰，快速响应需求',
+        children: '古典主义与秩序美学复现',
       },
     ],
   },
@@ -20,7 +20,7 @@ export const Feature80DataSource = {
       {
         name: 'button',
         className: 'feature8-button',
-        children: { href: '#', children: '立即体验' },
+        children: { href: '#', children: '电台播放' },
       },
     ],
   },
@@ -41,7 +41,7 @@ export const Feature80DataSource = {
           gutter: 120,
           title: {
             className: 'feature8-carousel-title-block',
-            children: '平台自主训练流程',
+            children: '巴拉克时期',
           },
           children: [
             {
@@ -52,7 +52,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -66,12 +66,15 @@ export const Feature80DataSource = {
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '巴赫',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
-                    children: '沟通业务需求，对接人：诚凡、芸彩',
+                    children: '巴赫的音乐表达了在此之前的人们不敢用、不能用或只是尝试用音乐表达却未成功的东西。' +
+                        '他用独一无二的现实主义手法创造了一种朴实无华的万能的语言，并借助这种语言描绘人的情感和世间百态，' +
+                        '他的音乐传达出一种思想，即认为音乐的受众不再仅仅是传统的基督教市民，而是孤独的、疑惑的现代人，' +
+                        '他们没有了信仰，在社会中找不到精神寄托，不得不在没有“教堂赐福”的情况下度过人生的绝大部分时光',
                   },
                 ],
               },
@@ -84,7 +87,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -98,13 +101,16 @@ export const Feature80DataSource = {
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '蒙特威尔第',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                      '他的音乐犹如人的躯体，富有鲜明的个性和炽烈的热情，从而将巴罗克音乐推向高潮。' +
+                        '他注意吸取前人的经验，巧妙地运用传统的复调音乐手法，把乐曲的形式固定在歌剧中，' +
+                        '并明确规定音乐的表现手法必须服从于戏剧情节的需要。他的歌剧感情色彩浓厚，重视对人物心理方面的刻画，' +
+                        '并首创以管弦乐队来伴奏歌剧，为音乐的展开提供了广阔的余地。',
                   },
                 ],
               },
@@ -117,7 +123,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -131,13 +137,15 @@ export const Feature80DataSource = {
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '柯瑞里',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                      '巴洛克时期意大利作曲家、小提琴演奏家。出生于福西格纳诺（位于今拉韦纳省）。' +
+                        '关于他的早年生活，人们知道得很少。他的小提琴老师是巴萨尼.马铁奥.西摩涅利；他的作曲教师则是教皇礼拜堂的一位著名歌手。' +
+                        '由科雷利所倡导的演奏方式，经过他和他的学生：比如杰米西尼亚、洛卡泰利等，继承发展后，成为非常重要的小提琴演奏技术。',
                   },
                 ],
               },
@@ -150,7 +158,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -164,13 +172,16 @@ export const Feature80DataSource = {
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '浦赛尔',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                      '亨利·普赛尔（HenryPurcell,1659-1695），英国作曲家。十岁起在皇室小圣堂中充当歌童，后改任皇室乐器保管员的助手，' +
+                        '先后当过皇室侍从乐队的作曲师、威斯特每斯特大教堂的管风琴师和国王的乐器管理员。作有歌剧、戏剧配乐、教堂音乐、' +
+                        '宫廷颂歌和宗教歌曲、民俗歌曲等。其中以歌剧《狄朵与伊尼阿斯》、为幕间歌舞表演所写的伴乐《女先知》、《印度女王》、' +
+                        '《暴风雨》，为德莱顿的爱国戏剧《亚瑟王》所作的配乐等较重要。\n',
                   },
                 ],
               },
@@ -182,19 +193,19 @@ export const Feature80DataSource = {
           className: 'feature8-block-row',
           gutter: 120,
           title: {
-            children: '平台自主训练流程',
+            children: '古典主义时期',
             className: 'feature8-carousel-title-block',
           },
           children: [
             {
               className: 'feature8-block-col',
-              md: 6,
+              md: 8,
               xs: 24,
               name: 'child0',
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -208,12 +219,132 @@ export const Feature80DataSource = {
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '海顿',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
-                    children: '沟通业务需求，对接人：诚凡、芸彩',
+                    children: '在海顿的交响曲中闪动着充满青春朝气的节奏性舞蹈旋律。它的交错、分散和重新组合，' +
+                        '所有这一切都是用高超的对位技巧来完成的。这样一个过程最后几乎不再是一个灵巧手段的结果，' +
+                        '而是成了根据幻想法则而进行舞蹈的一种诗性，它渗透到真正的乐观人生的肌肤里去',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 8,
+              xs: 24,
+              name: 'child1',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: '莫扎特',
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '莫扎特的音乐是生活的画像，但那是美化了的生活。旋律尽管是精神的反映，但它必须取悦于精神，而不伤及肉体或损害听觉。' +
+                        '所以，在莫扎特那里，音乐是生活和谐的表达。不仅他的歌剧，而且他所有的作品都是如此。他的音乐，无论看起来如何，' +
+                        '总是指向心灵而非智力，并且始终在表达情感或激情，但绝无令人不快或唐突的激情。',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 8,
+              xs: 24,
+              name: 'child2',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: '贝多芬',
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '贝多芬创作的九部交响乐，两首弥撒曲，还有不胜列举的序曲、协奏曲、奏鸣曲和弦乐四重奏曲，深深影响了后来作曲家的风格，' +
+                        '例如：交响乐《英雄》、《命运》、《田园》，第九交响乐的最后乐章《欢乐颂》；序曲《爱格蒙特》；第五号钢琴协奏曲《皇帝》；' +
+                        '《悲怆奏鸣曲》、《月光奏鸣曲》等等已是家喻户晓。他为人类留下了一笔宝贵财富，对世界音乐的发展也产生了巨大的影响，' +
+                        '因而被世人尊称为“乐圣”。',
+                  },
+                ],
+              },
+            },
+          ],
+        },
+        {
+          name: 'block2',
+          className: 'feature8-block-row',
+          gutter: 120,
+          title: {
+            children: '浪漫主义时期',
+            className: 'feature8-carousel-title-block',
+          },
+          children: [
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child0',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                        'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: '帕格尼尼',
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: '百多年以来帕格尼尼的作品始终没有离开过音乐会舞台，它们以明朗的配器、' +
+                        '鲜明的造型和满含深情而又永葆青春的旋律使人百听不厌。然而在今天也可能听得到这样的见解：只就表演而论，' +
+                        '帕格尼尼是个很了不起的伟人，至于说到他的创作，无非炫示绝技动人心目而已。在这方面，' +
+                        '看来他的命运倒和李斯特颇有类似的怀才不遇之处，李斯特的创作才能长期以来也是未曾得到大多数人的许可。' +
+                        '但是帕格尼尼一生之所以如此光辉而至今仍然不朽，恰恰证明他同时还是一位杰出的作曲家。',
                   },
                 ],
               },
@@ -226,7 +357,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                    'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -235,18 +366,20 @@ export const Feature80DataSource = {
                     name: 'image',
                     className: 'feature8-block-image',
                     children:
-                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                        'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
                   },
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '罗西尼',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                        '其以喜歌剧而名垂青史，其喜歌剧既有反映贫民生活的内容，又有对贵族浮华百态的描绘，内容生动、明快，人物的刻划个性鲜明。' +
+                        '由罗西尼开创的意大利歌剧时代，再经过多尼采蒂、贝利尼直至威尔第所形成的美声风格，使意大利至今仍为声乐艺术的圣殿，' +
+                        '他们对声乐艺术的贡献是无人能及的。',
                   },
                 ],
               },
@@ -259,7 +392,7 @@ export const Feature80DataSource = {
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                    'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -268,18 +401,20 @@ export const Feature80DataSource = {
                     name: 'image',
                     className: 'feature8-block-image',
                     children:
-                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                        'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
                   },
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '多尼采蒂',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                        '多尼采蒂的作品在风格上也很不一致。他早期所写的喜歌剧受罗西尼的影响。到巴黎后，法国大歌剧对他有所启示。' +
+                        '他的喜歌剧《爱的甘醇》仍然保持着意大利喜歌剧的传统风格,1933年所作的《吕克莱斯·波尔吉》则开始向浪漫主义进行探索。' +
+                        '他的正歌剧《拉美莫尔的露契亚》被认为是体现多尼采蒂独特风格的代表作，至今仍在各国舞台上演。',
                   },
                 ],
               },
@@ -288,11 +423,11 @@ export const Feature80DataSource = {
               className: 'feature8-block-col',
               md: 6,
               xs: 24,
-              name: 'child3',
+              name: 'child1',
               arrow: {
                 className: 'feature8-block-arrow',
                 children:
-                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+                    'https://gw.alipayobjects.com/zos/rmsportal/agOOBdKEIJlQhfeYhHJc.svg',
               },
               children: {
                 className: 'feature8-block-child',
@@ -301,18 +436,20 @@ export const Feature80DataSource = {
                     name: 'image',
                     className: 'feature8-block-image',
                     children:
-                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                        'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
                   },
                   {
                     name: 'title',
                     className: 'feature8-block-title',
-                    children: '需求沟通',
+                    children: '舒曼',
                   },
                   {
                     name: 'content',
                     className: 'feature8-block-content',
                     children:
-                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                        '罗伯特·舒曼是19世纪德国浪漫主义音乐的典型代表人物，他的浪漫主义精神主要是受到家庭和时代的影响而形成，' +
+                        '这个时代流行的唯心主义哲学、浪漫主义文学以及浪漫主义音乐塑造了浪漫主义音乐家舒曼。舒曼是属于十九世纪的，' +
+                        '也是属于后世的，舒曼的音乐创作和音乐评论，更重要的是他那种精神，对后世乐坛产生深刻而长远的影响。',
                   },
                 ],
               },
@@ -323,6 +460,7 @@ export const Feature80DataSource = {
     },
   },
 };
+/*
 export const Pricing10DataSource = {
   wrapper: { className: 'home-page-wrapper pricing1-wrapper' },
   page: { className: 'home-page pricing1' },
@@ -330,7 +468,7 @@ export const Pricing10DataSource = {
   titleWrapper: {
     className: 'pricing1-title-wrapper',
     children: [
-      { name: 'title', children: '价目表', className: 'pricing1-title-h1' },
+      { name: 'title', children: '音乐巨匠', className: 'pricing1-title-h1' },
     ],
   },
   block: {
@@ -436,3 +574,4 @@ export const Pricing10DataSource = {
   },
 };
 
+*/
